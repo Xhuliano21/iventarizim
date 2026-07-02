540d46276f02eb80eb78c4cf4f7a3acd229cd32f
Xhuliano21 <54474125+Xhuliano21@users.noreply.github.com>
2026-07-02 13:37:16 +0200

    Update client.js

--- magazina/frontend/src/api/client.js
+++ magazina/frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://iventarizim-api.onrender.com });
+const api = axios.create({ baseURL: "https://iventarizim-api.onrender.com/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
